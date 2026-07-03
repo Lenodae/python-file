48a0ab97b18659d2f2cbe423b553429d5b596850
L <2095928378@qq.com>
2026-07-03 12:25:16 +0800

    clean up

--- how.py
+++
@@ -1,6 +0,0 @@
-print("Hello,world!\n♡")
-print("*" * 10)
-print("Wow, it can really work here")
-print("Can I do this?")
-print("It seems like it works")
-
--- print('what fuck do they want!').py
+++
@@ -1,2 +0,0 @@
-print('what fuck do they want!')
-
